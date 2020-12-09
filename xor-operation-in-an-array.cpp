e20149e2a95e43650bab8f5daa69f806094a8215
Zachary Knopp <zachary@zknopp.com>
2020-12-08 22:01:27 -0400

    Create xor-operation-in-an-array.cpp

---
+++ xor-operation-in-an-array.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/xor-operation-in-an-array/
+
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ret = start;
+        
+        for(int i = 1; i < n; i++) {
+            ret = ret ^ (start + 2 * i);
+        }
+        
+        return ret;
+    }
+};
